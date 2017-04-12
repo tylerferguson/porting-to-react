9e866ceb37e8f18ced8363d7b132a404c8780532
Tyler Ferguson <tylerferguson235@gmail.com>
2017-04-12 17:22:13 +0100

    Added about and books routes. Serve a different app age from each route.

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.send('hi world!'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'about.html'))
+})
+
+app.get('/books', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'books.html'))
+})
 
 app.listen(8080, () => console.log('Server listening on 8080'))
